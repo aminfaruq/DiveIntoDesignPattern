6942097df39e8064024a0f026634e961b7f359e9
Amin Faruq <aminfaruk.fa@gmail.com>
2024-06-28 14:52:23 +0800

    Feat: Factory method conceptional
    implement factory method base one book

--- DiveIntoDesignPatternTests/Creational Design Pattern/Factory Method/FactoryMethodConceptualTests.swift
+++ DiveIntoDesignPatternTests/Creational Design Pattern/Factory Method/FactoryMethodConceptualTests.swift
@@ -6,8 +6,17 @@
 //
 
 import XCTest
+import DiveIntoDesignPattern
 
 final class FactoryMethodConceptualTests: XCTestCase {
-
-  
+    
+    func test_FactoryMethodConceptual() {
+        /// The Application picks a creator's type depending on the
+        /// configuration or environment
+        print("App: Launched with the ConcreteCreator1.")
+        Client.someClientCode(creator: ConcreteCreator1())
+        
+        print("\nApp: Launched with the ConcreteCreator2.")
+        Client.someClientCode(creator: ConcreteCreator2())
+    }
 }
