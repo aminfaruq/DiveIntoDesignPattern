6fbb31349bd6950e0c92785dd6c852d7efc6018d
Amin Faruq <aminfaruk.fa@gmail.com>
2024-06-28 14:09:46 +0800

    Initial commit

---
+++ DiveIntoDesignPattern/DiveIntoDesignPattern.h
@@ -0,0 +1,18 @@
+//
+//  DiveIntoDesignPattern.h
+//  DiveIntoDesignPattern
+//
+//  Created by Amin faruq on 28/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DiveIntoDesignPattern.
+FOUNDATION_EXPORT double DiveIntoDesignPatternVersionNumber;
+
+//! Project version string for DiveIntoDesignPattern.
+FOUNDATION_EXPORT const unsigned char DiveIntoDesignPatternVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiveIntoDesignPattern/PublicHeader.h>
+
+
