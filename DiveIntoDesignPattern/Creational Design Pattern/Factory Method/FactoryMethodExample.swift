5ffb88996af0ffd068534401f6422ba35fcbb00f
Amin Faruq <aminfaruk.fa@gmail.com>
2024-06-28 15:19:24 +0800

    Feat: Create example real word factory method

--- DiveIntoDesignPattern/Creational Design Pattern/Factory Method/FactoryMethodExample.swift
+++ DiveIntoDesignPattern/Creational Design Pattern/Factory Method/FactoryMethodExample.swift
@@ -6,3 +6,116 @@
 //
 
 import Foundation
+
+//MARK: - STEP 1 -
+protocol Projector {
+    
+    /// Abstract projector interface
+    
+    var currentPage: Int { get }
+    
+    func present(info: String)
+    
+    func sync(with projector: Projector)
+    
+    func update(with page: Int)
+}
+
+extension Projector {
+    /// Base implementation of Projector methods
+    
+    func sync(with projector: Projector) {
+        projector.update(with: currentPage)
+    }
+}
+
+//MARK: - STEP 2 -
+class WifiProjector: Projector {
+    var currentPage: Int = 0
+    
+    func present(info: String) {
+        print("Info is presented over Wifi: \(info)")
+    }
+    
+    func update(with page: Int) {
+        /// ... scroll page via WiFi connection
+        /// ...
+        currentPage = page
+    }
+}
+
+class BluetoothProjector: Projector {
+    var currentPage: Int = 0
+    
+    func present(info: String) {
+        print("Info is presented over Wifi: \(info)")
+    }
+    
+    func update(with page: Int) {
+        /// ... scroll page via WiFi connection
+        /// ...
+        currentPage = page
+    }
+}
+
+//MARK: - STEP 3 -
+protocol ProjectorFactory {
+    func createProjector() -> Projector
+    
+    func syncedProjector(with projector: Projector) -> Projector
+}
+
+extension ProjectorFactory {
+    
+    /// Base implementation of ProjectorFactory
+    func syncedProjector(with projector: Projector) -> Projector {
+        
+        /// Every instance creates an own projector
+        let newProjector = createProjector()
+        
+        /// sync projectors
+        newProjector.sync(with: projector)
+        
+        return newProjector
+    }
+}
+
+//MARK: - STEP 4 -
+class WifiFactory: ProjectorFactory {
+    
+    func createProjector() -> Projector {
+        return WifiProjector()
+    }
+}
+
+class BluetoothFactory: ProjectorFactory {
+    
+    func createProjector() -> Projector {
+        return BluetoothProjector()
+    }
+}
+
+internal class ClientCode {
+    private var currentProjector: Projector?
+    
+    func present(info: String, with factory: ProjectorFactory) {
+        /// Check wheater a client code already present smth...
+        
+        guard let projector = currentProjector else {
+            
+            /// 'currentProjector' variable is nil. Create a new projector and
+            /// start presentation.
+            
+            let projector = factory.createProjector()
+            projector.present(info: info)
+            self.currentProjector = projector
+            return
+        }
+        
+        /// Client code already has a projector. Let's sync pages of the old
+        /// projector with a new one.
+        
+        self.currentProjector = factory.syncedProjector(with: projector)
+        self.currentProjector?.present(info: info)
+    }
+}
--- DiveIntoDesignPatternTests/Creational Design Pattern/Factory Method/FactoryMethodExampleTests.swift
+++ DiveIntoDesignPatternTests/Creational Design Pattern/Factory Method/FactoryMethodExampleTests.swift
@@ -6,8 +6,19 @@
 //
 
 import XCTest
+import DiveIntoDesignPattern
 
 final class FactoryMethodExampleTests: XCTestCase {
 
-   
+    func test_FactoryMethodRealWorld() {
+        let info = "Very important info of the presentation"
+
+        let clientCode = ClientCode()
+
+        /// Present info over WiFi
+        clientCode.present(info: info, with: WifiFactory())
+
+        /// Present info over Bluetooth
+        clientCode.present(info: info, with: BluetoothFactory())
+    }
 }
