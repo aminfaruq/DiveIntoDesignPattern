00a0d4b79ee64578fec973d5bc2bc07f7773e880
Amin Faruq <aminfaruk.fa@gmail.com>
2025-02-28 17:32:07 +0800

    Feat: Composite real world

--- DiveIntoDesignPatternTests/Structural Design Pattern/Composite/CompositeExampleTests.swift
+++ DiveIntoDesignPatternTests/Structural Design Pattern/Composite/CompositeExampleTests.swift
@@ -8,6 +8,35 @@
 import XCTest
 
 final class CompositeExampleTests: XCTestCase {
-
-
+    
+    func testCompositeRealWorld() {
+        print("\nClient: Applying 'default' theme for 'NSButton'")
+        apply(theme: DefaultButtonTheme(), for: NSButton())
+        
+        print("\nClient: Applying 'night' theme for 'NSButton'")
+        apply(theme: NightButtonTheme(), for: NSButton())
+        
+        print("\nClient: Let's use View Controller as a composite!")
+        
+        print("\nClient: Applying 'night button' theme for 'WelcomeViewController'...")
+        apply(theme: NightButtonTheme(), for: WelcomeViewController())
+        print()
+        
+        print("\nClient: Applying 'night label' theme for 'WelcomeViewController'...")
+        apply(theme: NightLabelTheme(), for: WelcomeViewController())
+        print()
+        
+        print("\nClient: Applying 'default button' theme for 'WelcomeViewController'...")
+        apply(theme: DefaultButtonTheme(), for: WelcomeViewController())
+        print()
+        
+        print("\nClient: Applying 'default label' theme for 'WelcomeViewController'...")
+        apply(theme: DefaultLabelTheme(), for: WelcomeViewController())
+        print()
+    }
+    
+    func apply<T: Theme>(theme: T, for component: CompositeRealWorldComponent) {
+        component.accept(theme: theme)
+    }
+    
 }
